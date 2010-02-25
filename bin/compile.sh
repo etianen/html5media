0a6182e22fcf9b3ea7b8c99909776001132ec4de
Dave Hall <dave@etianen.com>
2010-02-25 10:41:41 +0000

    Added in support for Theora. Maybe.
    git-svn-id: https://html5media.googlecode.com/svn/trunk@58 0226dde6-208f-11df-8982-67e453f37615

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-cat ../src/flowplayer.js > ../src/jquery.html5media.min.js
-cat ./header.js >> ../src/jquery.html5media.min.js
-java -jar compiler.jar --js ../src/jquery.html5media.js >> ../src/jquery.html5media.min.js
+java -jar compiler.jar --js ../src/jquery.html5media.js > ../src/jquery.html5media.min.js
