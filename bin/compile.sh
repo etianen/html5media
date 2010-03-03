0b5c540f2c579b7f9f8acbd1bb3d2d393fbc3e86
Dave Hall <dave@etianen.com>
2010-03-03 22:06:28 +0000

    Adding in compatibility fix for old jQuery-dependant version.
    git-svn-id: https://html5media.googlecode.com/svn/trunk@93 0226dde6-208f-11df-8982-67e453f37615

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-#java -jar compiler.jar --js ../src/flowplayer.js --js ../src/domready.js --js ../src/html5media.js > ../src/html5media.min.js
-cat ../src/flowplayer.js ../src/domready.js ../src/html5media.js > ../src/html5media.min.js
+java -jar compiler.jar --js ../src/flowplayer.js --js ../src/domready.js --js ../src/html5media.js > ../src/html5media.min.js
+java -jar compiler.jar --js ../src/flowplayer.js --js ../src/domready.js --js ../src/html5media.js --js ../src/jquery.html5media.js > ../src/jquery.html5media.min.js
