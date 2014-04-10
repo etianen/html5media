02fd144ec6cbc4ec5d570c71fb6e8038ca8978a4
Dave Hall <dave@etianen.com>
2014-04-10 09:13:41 +0100

    Building the js api using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,24 @@
-var gulp = require("gulp");
+"use strict";
 
+var meta = require("./package.json");
+var gulp = require("gulp");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var concat = require("gulp-concat");
+
+
+var API_BUILD_ROOT = "build/api/" + meta.version;
+
+
+gulp.task("api-js", function() {
+    return gulp.src([
+        "lib/flowplayer/flowplayer.js",
+        "lib/domready/domready.js",
+        "src/api/html5media.js",
+    ])
+    .pipe(concat("html5media.js"))
+    .pipe(gulp.dest(API_BUILD_ROOT))
+    .pipe(uglify())
+    .pipe(rename("html5media.min.js"))
+    .pipe(gulp.dest(API_BUILD_ROOT));
+});
