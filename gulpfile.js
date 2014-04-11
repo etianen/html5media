b83973e50946904d9d18f6d337339c5dd9daa10d
Dave Hall <dave@etianen.com>
2014-04-11 13:35:38 +0100

    Building media and www files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 "use strict";
 
 var meta = require("./package.json");
+var path = require("path");
 var gulp = require("gulp");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var concat = require("gulp-concat");
 var clean = require("gulp-clean");
+var inject = require("gulp-inject");
+var minifyCSS = require("gulp-minify-css");
 
 
-var API_BUILD_ROOT = "build/api/" + meta.version;
+var API_JS_BUILD_ROOT = path.join("build", "api", meta.version);
 
 
 gulp.task("clean", function() {
@@ -23,15 +26,15 @@ gulp.task("clean", function() {
 
 gulp.task("api-js", function() {
     return gulp.src([
-        "lib/flowplayer/flowplayer.js",
-        "lib/domready/domready.js",
+        "src/lib/flowplayer/flowplayer.js",
+        "src/lib/domready/domready.js",
         "src/api/html5media.js"
     ])
     .pipe(concat("html5media.js"))
-    .pipe(gulp.dest(API_BUILD_ROOT))
+    .pipe(gulp.dest(API_JS_BUILD_ROOT))
     .pipe(uglify())
     .pipe(rename("html5media.min.js"))
-    .pipe(gulp.dest(API_BUILD_ROOT));
+    .pipe(gulp.dest(API_JS_BUILD_ROOT));
 });
 
 
@@ -42,11 +45,54 @@ gulp.task("api-swf", function() {
         "lib/flowplayer.audio/flowplayer.audio.swf",
         "lib/swfobject/expressInstall.swf"
     ])
-    .pipe(gulp.dest(API_BUILD_ROOT))
+    .pipe(gulp.dest(API_JS_BUILD_ROOT));
 });
 
 
 gulp.task("api", ["api-js", "api-swf"]);
 
 
-gulp.task("build", ["api"]);
+gulp.task("media-css", function() {
+    return gulp.src([
+        "src/media/styles.css"
+    ])
+    .pipe(gulp.dest("build/media"))
+    .pipe(minifyCSS({
+        compatibility: "ie8"
+    }))
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest("build/media"));
+});
+
+
+gulp.task("media-static", function() {
+    return gulp.src([
+        "src/media/*",
+        "!src/media/*.css",
+    ])
+    .pipe(gulp.dest("build/media"));
+});
+
+
+gulp.task("media", ["media-static", "media-css"]);
+
+
+gulp.task("www", ["api"], function() {
+    return gulp.src([
+        "./src/www/*.html"
+    ])
+    .pipe(inject(gulp.src([
+        path.join(API_JS_BUILD_ROOT, "html5media.min.js"),
+        "build/media/styles.min.css"
+    ], {
+        read: false
+    }), {
+        ignorePath: "/build",
+        addRootSlash: false,
+        addPrefix: ".."
+    }))
+    .pipe(gulp.dest("build/www"));
+});
+
+
+gulp.task("build", ["api", "media", "www"]);
