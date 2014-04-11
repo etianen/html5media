ee1642e57527d50081ba51d289e92d2f1e36674d
Dave Hall <dave@etianen.com>
2014-04-11 13:07:04 +0100

    SWF API build step.

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,27 @@ var gulp = require("gulp");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var concat = require("gulp-concat");
+var clean = require("gulp-clean");
 
 
 var API_BUILD_ROOT = "build/api/" + meta.version;
+
+
+gulp.task("clean", function() {
+    return gulp.src([
+        "build"
+    ], {
+        "read": false
+    })
+    .pipe(clean());
+});
 
 
 gulp.task("api-js", function() {
     return gulp.src([
         "lib/flowplayer/flowplayer.js",
         "lib/domready/domready.js",
-        "src/api/html5media.js",
+        "src/api/html5media.js"
     ])
     .pipe(concat("html5media.js"))
     .pipe(gulp.dest(API_BUILD_ROOT))
@@ -22,3 +33,20 @@ gulp.task("api-js", function() {
     .pipe(rename("html5media.min.js"))
     .pipe(gulp.dest(API_BUILD_ROOT));
 });
+
+
+gulp.task("api-swf", function() {
+    return gulp.src([
+        "lib/flowplayer/flowplayer.swf",
+        "lib/flowplayer/flowplayer.controls.swf",
+        "lib/flowplayer.audio/flowplayer.audio.swf",
+        "lib/swfobject/expressInstall.swf"
+    ])
+    .pipe(gulp.dest(API_BUILD_ROOT))
+});
+
+
+gulp.task("api", ["api-js", "api-swf"]);
+
+
+gulp.task("build", ["api"]);
